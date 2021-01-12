f2007f15231c8ac778be549d4cfa6069f1399524
Thanakun <Thanakun_Ch@cmu.ac.th>
2021-01-12 17:22:39 +0700

    :)

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    int x = 0;
+    string i[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int a = 0; a < 3; a++){
+        cin.get();
+    }
+    x = rand() % 9;
+    cout << "You will get "<< i[x] <<" in this 261102.";
+}
